6703624a4d2cd69509f18fb0ff75ade79013fd7a
MD Ariful Islam <ariful.islam@stud.uni-due.de>
2017-09-13 14:27:51 +0200

    node js tute and creativesoft4u project added

---
+++ Project works/creativesoft4u/application/modules/mainsite/views/front_page/front_view.php
@@ -0,0 +1,229 @@
+<?php $this->load->view('front_page/slogan_view');?>
+<?php $this->load->view('front_page/service_view');?>
+<?php $this->load->view('front_page/portfolio_view');?>
+<?php $this->load->view('front_page/about_view');?>
+<?php $this->load->view('front_page/team_view');?>
+<?php $this->load->view('front_page/clients_view');?>
+<?php $this->load->view('front_page/contact_view');?>
+
+<!-- Portfolio Modals -->
+    <!-- Use the modals below to showcase details about your portfolio projects! -->
+
+    <!-- Portfolio Modal 1 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <h2>Cyomed GmBh</h2>
+                                <p class="item-intro text-muted">A fully developed medical website for doctors& patients.</p>
+                                <img class="img-responsive img-centered" src="<?php echo base_url('assets/img/portfolio/cyomed.png')?>" alt="">
+                                <p>
+                                    <strong>Objectives of the project:</strong> To develop smart medical application and services via online to help doctors and patients and reduce communication gaps between then. Now we have all latest devices and applications running whole world and we hope we could manage and give more smarter and easiest service platform for people all around to help them keep track their daily heaths activities, reports and care.  
+                                </p>
+                                <p>
+                                    <strong>Programming used:</strong> PHP5, Javascripts, Html, Css, Jquery etc.<br>
+                                    <strong>DataBase used:</strong> MySQL<br>
+                                    <strong>Framework&layout used:</strong> Codeigniter 2.0, Twitter Bootstrap<br>
+                                    <strong>Others used:</strong> Highcharts, Quickbox video platform, Google APIs, Googlefit, Jawbon etc.<br>
+                                </p>
+                                <ul class="list-inline">
+                                    <li>Client: Cyomed GmBh, Duesseldorf, Germany</li>
+                                    <li>Category: Telemedicine</li>
+                                </ul>
+                                <button type="button" class="btn btn-success"><i class="folder-open"></i><a href="https://cyomed.com/">Check details</a></button>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Portfolio Modal 2 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <h2>Dhong.com</h2>
+                                <p class="item-intro text-muted">
+                                	A fully developed E-commerce site for online shopping.
+                                </p>
+                                <img class="img-responsive img-centered" src="<?php echo base_url('assets/img/portfolio/coming.jpg')?>" alt="">
+                                <p>
+                                    <strong>Objectives of the project:</strong>
+                                    Online Shopping site to buy and sell products using different types of transaction like mastercard, paypal, bank transfer, Visa etc. User can easily Buy things using log-in ID as single click options or without log-in as a guest customers. The product will be sent to customers address immediately after successfully done transaction or pay on delivery options also.  
+                                </p>
+                                <p>
+                                    <strong>Programming used:</strong> PHP5, Javascripts, Html, Css, Jquery etc.<br>
+                                    <strong>DataBase used:</strong> MySQL<br>
+                                    <strong>Framework&layout used:</strong> Codeigniter 2.0, Twitter Bootstrap<br>
+                                    <strong>Others used:</strong> coming...<br>
+                                </p>
+                                <ul class="list-inline">
+                                    <li>Client: Dhong Ltd, dhaka, Bangladesh</li>
+                                    <li>Category: E-commerce</li>
+                                </ul>
+                                <button type="button" class="btn btn-success"><i class="folder-open"></i><a href="https://dhong.com/">Check details</a></button>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Portfolio Modal 3 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <h2>#HashCause</h2>
+                                <p class="item-intro text-muted">
+                                	A fully developed Dynamic Website for Software Business.
+                                </p>
+                                <img class="img-responsive img-centered" src="<?php echo base_url('assets/img/portfolio/hashcause.png')?>" alt="">
+                                <p>
+                                    <strong>Objectives of the project:</strong> 
+                                    With over 5 years under our belt, we are the PERFECT contractor for your project. We specialize in building application for small to medium size businesses, from scratch to production version application. We have the skills and professionalism to develop the online presence your business NEEDS. We pride ourselves on providing the highest quality of work, and will never complete a project until the client is 100 percent satisfied.
+                                    We provide a full spectrum of custom software services, with a focus on web,mobile application development and database decision and design. Our offerings cover all stages of the software development life cycle: from business analysis, design, and prototyping to the actual development, quality assurance, and post-project maintenance and support.
+                                    Our team of designers and engineers works with clients to build products and create engaging experiences for emerging technologies. To compete in a field as complex and rapidly changing as mobile development we emphasize methodology and hands-on skills. Our engineers and designers work together, encouraging cross-training and an open minded approach to tasks. We give back by creating and contributing to open source projects.
+
+                                </p>
+                                <p>
+                                    <strong>Programming used:</strong> PHP5, Javascripts, Html, Css, Jquery etc.<br>
+                                    <strong>DataBase used:</strong> MySQL&MongoDB<br>
+                                    <strong>Framework&layout used:</strong> Codeigniter 2.0, Twitter Bootstrap<br>
+                                    <strong>Others used:</strong> font awesome, brainwave etc<br>
+                                </p>
+                                <ul class="list-inline">
+                                    <li>Client: HashCause, Bonn, Germany</li>
+                                    <li>Category: Web Design</li>
+                                </ul>
+                                <button type="button" class="btn btn-success"><i class="folder-open"></i><a href="http://hashcause.com/">Check details</a></button>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Portfolio Modal 4 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <h2>Payment Module</h2>
+                                <p class="item-intro text-muted">Coming Soon....</p>
+                                <img class="img-responsive img-centered" src="<?php echo base_url('assets/img/portfolio/coming.jpg')?>" alt="">
+                                <p>On Going.....</p>
+                                <p>Check later.....</p>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Portfolio Modal 5 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <h2>Appointment System</h2>
+                                <p class="item-intro text-muted">Coming soon....</p>
+                                <img class="img-responsive img-centered" src="<?php echo base_url('assets/img/portfolio/coming.jpg')?>" alt="">
+                                <p>On going....</p>
+                                <p>Check later.....</p>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Portfolio Modal 6 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <h2>Video and Text Chat bot</h2>
+                                <p class="item-intro text-muted">Coming soon....</p>
+                                <img class="img-responsive img-centered" src="<?php echo base_url('assets/img/portfolio/coming.jpg')?>" alt="">
+                                <p>On going....</p>
+                                <p>Check later....</p>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ Project works/creativesoft4u/application/modules/mainsite/views/front_page/slogan_view.php
@@ -0,0 +1,11 @@
+<!-- Header slogan-->
+    <header>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">A Great Mind Can Give A Great World!</div>
+                <div class="intro-heading">I am glad to meet you</div>
+                <a href="#services" class="page-scroll btn btn-xl">Ask Me More</a>
+            </div>
+        </div>
+    </header>
+<!-- Header slogan ends-->
